b4afcf9e3d97b4da9bc4f3ffe7fcb1ee2ee1d0ab
nikhilkumarsingh <nikhilksingh97@gmail.com>
2017-04-01 22:27:31 +0530

    Initial commit

---
+++ news_notifier.py
@@ -0,0 +1,27 @@
+import time
+import notify2
+from topnews import topStories
+
+
+# path to notification window icon
+ICON_PATH = "/home/nikhil/Dropbox/my_workspace/study_material&references/python_support/news_icon.png"
+
+# fetch news items
+newsitems = topStories()
+
+# initialise the d-bus connection
+notify2.init("News Notifier")
+# create Notification object
+n = notify2.Notification(None, icon = ICON_PATH)
+# set urgency level
+n.set_urgency(notify2.URGENCY_NORMAL)
+# set timeout for a notification
+n.set_timeout(10000)
+
+for newsitem in newsitems:
+    # update notification data for Notification object 
+    n.update(newsitem['title'], newsitem['description'])
+    # show notification on screen
+    n.show()
+    # short delay between notifications
+    time.sleep(15)
