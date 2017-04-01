ab0a4a41a0da11be94369dde2378f9b5fb4cc8fc
nikhilkumarsingh <nikhilksingh97@gmail.com>
2017-04-02 05:14:11 +0530

    added download link

--- win-example.py
+++ win-example.py
@@ -1,4 +1,9 @@
 # alternative for windows
+# download notify-send from here: http://vaskovsky.net/notify-send/notify-send.zip
+# unzip the file and add notify-send.exe to environment variables OR
+# keep notify-send.exe in same folder where this python script is kept.
+
+
 from subprocess import call
 
 call(["notify-send", "This is title!", "This is message body!"])
